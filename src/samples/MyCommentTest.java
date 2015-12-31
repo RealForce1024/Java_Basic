c53c5a73781ead2b2bf42d06e0dd3e66c003c12c
fqc <337940626@qq.com>
2015-12-31 19:25:48 +0800

    添加注释模板

---
+++ src/samples/MyCommentTest.java
@@ -0,0 +1,17 @@
+package samples;
+
+
+/**
+ * ${PACKAGE_NAME}
+ * @author ${USER}
+ * @date ${DATE}
+ */
+
+/**
+ * samples
+ *
+ * @author fqc
+ * @date 15/12/31
+ */
+public class MyCommentTest {
+}
