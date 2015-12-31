5ed85a991bfc4897d68449d72515e208eda5be16
fqc <337940626@qq.com>
2015-12-31 19:01:05 +0800

    注意random随机数据的范围

--- src/samples/GuessNumber.java
+++ src/samples/GuessNumber.java
@@ -1,5 +1,6 @@
 package samples;
 
+import java.util.Random;
 import java.util.Scanner;
 
 
@@ -8,18 +9,30 @@ import java.util.Scanner;
  */
 public class GuessNumber {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int random_num = (int) (Math.random()*10);
-        int guess_num = 0;
-
-        do {
-            System.out.println("Please input one num of [0~9] ");
-            guess_num = scanner.nextInt();
-            System.out.println("guess_num = " + guess_num);
-            System.out.println("random_num = " + random_num);
-        }while(random_num != guess_num);
+//        Scanner scanner = new Scanner(System.in);
+//        int random_num = (int) (Math.random()*10);
+//        int guess_num = 0;
+//
+//        do {
+//            System.out.println("Please input one num of [0~9] ");
+//            guess_num = scanner.nextInt();
+//            System.out.println("guess_num = " + guess_num);
+//            System.out.println("random_num = " + random_num);
+//        }while(random_num != guess_num);
+        guessNumber();
     }
 
+
+    private static void guessNumber() {
+        int guessNum;
+        int randomNum;
+        do {
+            System.out.println("请输入您心中的数");
+            guessNum = new Scanner(System.in).nextInt();
+            randomNum = (int)(Math.random()*10);//random 随机数0.xxxxx.~1
+            System.out.println("猜数:"+guessNum+"随机数:"+randomNum);
+        } while (guessNum!=randomNum);
+    }
 
 
 
