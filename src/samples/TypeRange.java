bd2b28d3c99ce065d61f608952c50a5116d11488
fqc <337940626@qq.com>
2016-02-14 15:53:15 +0800

    测试基本类型范围及简单字符编码

--- src/samples/TypeRange.java
+++ src/samples/TypeRange.java
@@ -1,5 +1,6 @@
 package samples;
 
+import java.io.UnsupportedEncodingException;
 import java.util.DoubleSummaryStatistics;
 
 /**
@@ -7,21 +8,25 @@ import java.util.DoubleSummaryStatistics;
  */
 public class TypeRange {
     public static void main(String[] args) {
-        //byte short int long 的范围
+
+        testByte();
+    }
+
+    public static void testTypeRange() {
+        //byte short int long 整数的范围
         System.out.printf("%d~%d%n", Byte.MIN_VALUE, Byte.MAX_VALUE);
         System.out.printf("%d~%d%n", Short.MIN_VALUE, Short.MAX_VALUE);
         System.out.printf("%d~%d%n", Integer.MIN_VALUE, Integer.MAX_VALUE);
         System.out.printf("%d~%d%n", Long.MIN_VALUE, Long.MAX_VALUE);
-        System.out.printf("%d~%d%n", Byte.MIN_VALUE, Byte.MAX_VALUE);
 
-        //float double 的精度范围
+        //float double 浮点数的精度范围
         System.out.printf("%d~%d%n", Float.MIN_EXPONENT, Float.MAX_EXPONENT);
         System.out.printf("%d~%d%n", Double.MAX_EXPONENT, Double.MAX_EXPONENT);
 
-        //char可表示的Unicode 范围
+        //char可表示的Unicode 字符范围
         System.out.printf("%h~%h%n", Character.MIN_VALUE, Character.MAX_VALUE);
 
-        //boolean的两个值
+        //boolean 布尔型的两个值
         System.out.printf("%b~%b%n", Boolean.FALSE, Boolean.TRUE);
 
         /*
@@ -36,6 +41,25 @@ public class TypeRange {
         0~ffff
         false~true
         * */
-
     }
+
+    public static void testByte() {
+        String str1 = "中国";
+        Character str2 = 'a';
+        try {
+            System.out.println(str1.getBytes("gb2312").length);
+            System.out.println(str1.getBytes("gbk").length);
+            System.out.println(str1.getBytes("utf-8").length);//默认utf-8
+
+//            System.out.println(str2.getBytes("gb2312").length);
+//            System.out.println(str2.getBytes("gbk").length);
+            System.out.println(str2.SIZE/8);//默认utf-8
+            System.out.println(Character.charCount(str2));
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
