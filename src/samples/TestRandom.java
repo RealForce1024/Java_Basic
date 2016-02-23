c92e6977a7cf63dacfbfced9a6ddf6dec6ff8eb4
fqc <337940626@qq.com>
2016-02-23 10:49:55 +0800

    默认是只用了当前系统的时间戳，random seed肯定不同了。

---
+++ src/samples/TestRandom.java
@@ -0,0 +1,25 @@
+package samples;
+
+/**
+ * samples
+ *
+ * @author fqc
+ * @date 16/2/23
+ */
+
+import java.util.Random;
+
+class TestRandom {
+    public static void main(String[] args) {
+//        Random random1 = new Random(100);
+        Random random1 = new Random();
+        System.out.println(random1.nextInt());
+        System.out.println(random1.nextFloat());
+        System.out.println(random1.nextBoolean());
+//        Random random2 = new Random(100);
+        Random random2 = new Random(); //默认是只用了当前系统的时间戳，random seed肯定不同了。
+        System.out.println(random2.nextInt());
+        System.out.println(random2.nextFloat());
+        System.out.println(random2.nextBoolean());
+    }
+}
