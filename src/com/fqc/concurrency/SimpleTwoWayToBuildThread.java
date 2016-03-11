5731aad457c45c7154d960cae8d64957c0e6e6b9
fqc <337940626@qq.com>
2016-03-11 17:04:30 +0800

    增加线程名称构造(内部类需要) ,run()方法调用后的结果 强调run方法不可直接在Thread类中调用

--- src/com/fqc/concurrency/SimpleTwoWayToBuildThread.java
+++ src/com/fqc/concurrency/SimpleTwoWayToBuildThread.java
@@ -1,38 +1,54 @@
 package com.fqc.concurrency;
 
+import com.fqc.io.IOUtils;
+
 /**
  * Created by fqc on 2016/1/4.
  */
 public class SimpleTwoWayToBuildThread {
 
-
-
-
     public static void main(String[] args) {
         //1,2
-        MyThread myThread = new MyThread();
-        MyThread myThread2 = new MyThread();
+        MyThread myThread = new MyThread("Thread-1");
+        MyThread myThread2 = new MyThread("Thread-2");
+
+
         myThread.start();
         myThread2.start();
+
+        //以下方式是将两线程类当成是普通类来处理。
+        //结果是 main-1-100 *2
+        //切不可直接调用线程类的run方法，而是通过start方法来调用。
+        myThread.run();
+        myThread2.run();
     }
-
-
 
 }
 
 class MyThread extends Thread {
-//    private int a = 0; //1
-    @Override
-    public void run(){
-        int a = 0; //2
 
+
+    private int a = 0; //1
+
+    public MyThread(String name) {
+        super(name);
+    }
+
+    public MyThread() {
+
+    }
+
+    @Override
+    public void run() {
+        //int a = 0; //2
         for (int i = 0; i < 100; i++) {
             try {
-                Thread.sleep((long) (Math.random()*1000));
+                Thread.sleep((long) (Math.random() * 1000));
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println(a++);
+            System.out.println(this.currentThread().getName() + "---" + a++);
+
         }
     }
 }
