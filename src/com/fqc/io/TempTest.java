5addc4faed810dbf1668769bda3a951964ae4e5e
fqc <337940626@qq.com>
2016-03-17 10:39:24 +0800

    move some file

---
+++ src/com/fqc/io/TempTest.java
@@ -0,0 +1,15 @@
+package com.fqc.io;
+
+import com.fqc.io.utils.IOUtils;
+
+/**
+ * com.fqc.io
+ *
+ * @author fqc
+ * @date 16/3/17
+ */
+public class TempTest {
+    public static void main(String[] args) {
+        System.out.println(IOUtils.getProjectPath());
+    }
+}
