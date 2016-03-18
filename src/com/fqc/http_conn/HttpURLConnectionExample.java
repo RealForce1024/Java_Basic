fd22efbc61b758e79ef9c02f4e29eecfd6454344
fqc <337940626@qq.com>
2016-03-18 11:08:04 +0800

    urlconnection post data with authorizaion but now return 400

---
+++ src/com/fqc/http_conn/HttpURLConnectionExample.java
@@ -0,0 +1,104 @@
+package com.fqc.http_conn;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
+
+public class HttpURLConnectionExample {
+
+    private final String USER_AGENT = "Mozilla/5.0";
+
+    public static void main(String[] args) throws Exception {
+
+        HttpURLConnectionExample http = new HttpURLConnectionExample();
+
+        System.out.println("Testing 1 - Send Http GET request");
+        http.sendGet();
+
+        System.out.println("\nTesting 2 - Send Http POST request");
+        http.sendPost();
+
+    }
+
+    // HTTP GET request
+    private void sendGet() throws Exception {
+
+        String url = "http://www.baidu.com/search?q=github.com/fqc";
+
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        // optional default is GET
+        con.setRequestMethod("GET");
+
+        //add request header
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        System.out.println(response.toString());
+
+    }
+
+    // HTTP POST request
+    private void sendPost() throws Exception {
+
+        String url = "https://ussouthcentral.services.azureml.net/workspaces/2dad6072409241e89b5868a22adced99/services/6c03112ec20d4a5bab652cdfff6e9185/score";
+        URL obj = new URL(url);
+        HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+        //add reuqest header
+        con.setRequestMethod("POST");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+        con.setRequestProperty("Content-Type","application/json");
+        con.setRequestProperty("Authorization","Bearer 4/PLANzPEabQgYYTbbmXNNyPShg8HZkqYzG5rqWADt/O7IlR7erF1f9B+1kuyr79og35CD/4yPV2CYvkMqbUzw==");
+
+
+        String urlParameters = "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345";
+
+        // Send post request
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(urlParameters);
+        wr.flush();
+        wr.close();
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'POST' request to URL : " + url);
+        System.out.println("Post parameters : " + urlParameters);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        System.out.println(response.toString());
+
+    }
+
+}
