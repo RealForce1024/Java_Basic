7fc7bd0d3cebb9aec069855ab461b6fed846eea1
fqc <337940626@qq.com>
2017-09-28 00:22:57 +0800

    lambda -> andThen

---
+++ src/main/java/com/fqc/jdk8/test02.java
@@ -0,0 +1,13 @@
+package com.fqc.jdk8;
+
+import java.util.Arrays;
+import java.util.function.IntConsumer;
+
+public class test02 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        IntConsumer outPrint = System.out::println;
+        IntConsumer errPrint = System.err::println;
+        Arrays.stream(arr).forEach(outPrint.andThen(errPrint));
+    }
+}
